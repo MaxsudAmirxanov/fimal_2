a44b195413ddfb7a2d5bf3d585bb1f30938ffad0
Maxsud <mamirxanov6@gmail.com>
2022-04-09 21:50:15 +0400

    rewrote the logic of checking the response, took out the read_random_word method

--- main.py
+++ main.py
@@ -4,63 +4,37 @@ class Game:
     def __init__(self):
 
         self.incorrect_letters = []
-        self.asked_question = {}
-        self.data_words = []
-        self.guessed_letters = []
+        self.guessed_letters_user = []
         self.actual_word = []
-        self.main_word = self.data_words[random.randint(0, len(self.data_words) - 1)] 
-    def reading_random_word(self):   
-        "Чтеное рандомного слова"
-        for i in open(f"word.txt", 'r', encoding="utf-8"):   
-            words = i.split('\n')
-            self.data_words.append(words[0])
+        self.main_word = []
 
-    def create_dict_for_word(self):
-        "Создание словарика"
-        # main_word = self.data_words[random.randint(0, len(self.data_words) - 1)]
-        for number, letters in enumerate(self.main_word):
-            self.asked_question[number] = {letters: False}
-
-
-    def checking_for_the_number_of_letters(self, user_ansver):
-        "Проверка слово, на количество букв"
-        if len(list(user_ansver)) == 1:
-            return True
-        else:
-            return False
-
-
-
-
-
-
-    def checking_question(self, user_ansver):
-        "Проверка ответа"
-        # main_word = self.create_dict_for_word()
-        if len(list(user_ansver)) == 1:
-            for number, letters in self.asked_question.items():
-                for letter, enter in letters.items():
-                    if user_ansver == letter or user_ansver in 'йиеё': #and letter == 'и' or user_ansver == 'и' and letter == 'й' or user_ansver == 'е' and letter == 'ё' or user_ansver == 'ё' and letter == 'е':
-                        self.asked_question[number] = {letter: True}
-                        self.guessed_letters.append(user_ansver)
-
-            if user_ansver not in list(self.main_word) and user_ansver not in self.incorrect_letters:
-                self.incorrect_letters.append(user_ansver)
-        else:
-            return False
-
-    def ask_question(self):
-        "Задать вопрос"
+    def reset_the_letter(self):
+        "Спросить букву" 
+        print(self.main_word[0])
         self.actual_word.clear()
-        for number, letters in self.asked_question.items():
-            for letter, enter in letters.items():
-                if enter == False:
-                    self.actual_word.append('__')
-                else:
-                    self.actual_word.append(letter)
-        full_word = ' '.join(self.actual_word)
-        
-        return full_word
+        for letter in list(self.main_word[0]):
+            if letter not in self.guessed_letters_user:
+                self.actual_word.append('__')
+            else:
+                self.actual_word.append(letter)
+
+        return ' '.join(self.actual_word)  
+
+    def update_game_status(self, user_answer):
+        'Обновление состояние игры'             
+    
+        for letter in self.main_word[0]:
+            if user_answer == letter or user_answer in 'ий' and letter in 'йи' or user_answer in 'её' and letter in 'её':
+                self.guessed_letters_user.append(user_answer)
+
+        if user_answer not in list(self.main_word[0]) and user_answer not in self.incorrect_letters and user_answer not in self.guessed_letters_user :
+            self.incorrect_letters.append(user_answer)
+
+    def checking_for_winnings(self):
+
+        full_word = self.reset_the_letter()
+        if '_' not in full_word:
+            return True
 
 class Interface:
     def __init__(self, user):
@@ -68,7 +42,7 @@ class Interface:
 
     def hidden_word(self):
         "Загаданное слово"
-        full_word = self.user.ask_question()
+        full_word = self.user.reset_the_letter()
         print('\n')
         print(f'Слово: {full_word}')
 
@@ -81,28 +55,40 @@ class Interface:
         "Ошибки пользователей"
         print(f'Ошибки ({len(self.user.incorrect_letters)}): {", ".join(self.user.incorrect_letters)}')
 
-    def number_of_errors(self):
-        'Количество ошибок'
-        number_attempts = 7 - len(user_1.incorrect_letters) 
+    def number_of_attempts(self):
+        'Количество попыток'
+        number_attempts = 7 - len(self.user.incorrect_letters) 
         print(f'Количество попыток: {number_attempts}')
 
     def entering_the_next_letter(self):
         "Вывод следующей буквы"
-        user_ansver = input('Введите следующую букву: ')
-        if self.user.checking_question(user_ansver) == False:
-            print('Введите букву !')
+        self.user.user_answer = input('Введите следующую букву: ')
+        return self.user.user_answer[0]
+    
+    def cheking_for_exit(self):
+        if self.user.checking_for_winnings() == True:
+            self.hidden_word()
+            self.picture_gallows()
+            print(f'Слово {self.user.main_word[0]} отгаданно, вы выиграли ))')
+            exit()
 
 
-user_1 = Game()  
-console = Interface(user_1)
-user_1.reading_random_word()
-user_1.create_dict_for_word()
+user = Game()  
+console = Interface(user)
 
+def read_random_word():
+    'Вывод рандоиного слова'  
+    with open('word.txt', 'r', encoding='utf-8') as file:
+        line = file.readlines()
+        user.main_word = line[random.randint(0, len(line) - 1)].split('\n')
+
+read_random_word()
 loop = True
 while loop:
+    console.cheking_for_exit()
     console.hidden_word()
     console.picture_gallows()
     console.user_errors()
-    console.number_of_errors()
-    console.entering_the_next_letter()
-
+    console.number_of_attempts()
+    user_answer = console.entering_the_next_letter()
+    user.update_game_status(user_answer)
